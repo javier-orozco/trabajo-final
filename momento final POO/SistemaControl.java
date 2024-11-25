f2dc66ea24838aeefb828246c1835b536baeea8a
javier-orozco <javier.orozco@campusucc.edu.co>
2024-11-25 17:26:42 -0500

    Add files via upload

---
+++ momento final POO/SistemaControl.java
@@ -0,0 +1,15 @@
+public class SistemaControl extends ComponenteAscensor {
+
+    public SistemaControl() {
+        super("Sistema de Control");
+    }
+
+    
+    public void operar() {
+        System.out.println("El " + getNombre() + " está controlando el ascensor.");
+    }
+
+    public void verificarFuncionamiento() {
+        System.out.println("El " + getNombre() + " está funcionando correctamente.");
+    }
+}
